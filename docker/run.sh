45589188188cdc893e0e68c7ab321d02d2db072b
AlexRedCross <belkill@mail.ru>
2024-08-09 21:58:24 +0300

    init empty project

--- docker/run.sh
+++ docker/run.sh
@@ -14,16 +14,6 @@ php artisan key:generate
 # link storage data
 php artisan storage:link
 
-# publish laravel-filemanager
-#php artisan vendor:publish --tag=lfm_public
-#php artisan vendor:publish --tag=lfm_config
-
-# publish telescope
-#php artisan vendor:publish --tag=telescope-migrations
-
-# make migration
-php artisan migrate --force
-
 chmod 664 -R /var/www/html/storage/logs
 
 /usr/bin/supervisord -c /etc/supervisord.conf
